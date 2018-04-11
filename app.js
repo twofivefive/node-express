c0e9919161adfa8b4b53b071fcdadaf3fb1214cd
Jan Rayman Arnaldo <jraymanarnaldo@gmail.com>
2018-04-11 15:21:02 +0000

    add local environment variables

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     commentsRoutes      = require("./routes/comments"),
     indexRoutes          = require("./routes/index");
     
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp"
+mongoose.connect(url);
 
-mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://admin:up56sv13xds2@ds053190.mlab.com:53190/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
