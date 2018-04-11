1c97a66f29e0fd099e1b5b9e11d4a5f84f4b744d
Jan Rayman Arnaldo <jraymanarnaldo@gmail.com>
2018-04-11 14:31:35 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://admin:up56sv13xds2@ds053190.mlab.com:53190/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
